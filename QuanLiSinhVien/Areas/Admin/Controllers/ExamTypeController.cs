1704da8cefa71f194363844779454be2bc816509
Nguyen Dong Huong <donghuong2000@users.noreply.github.com>
2020-12-08 22:36:47 +0700

    add exam type

---
+++ QuanLiSinhVien/Areas/Admin/Controllers/ExamTypeController.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using QuanLiSinhVien.Data;
+using QuanLiSinhVien.Models;
+
+namespace QuanLiSinhVien.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ExamTypeController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public ExamTypeController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult GetAll()
+        {
+            var obj = _db.ExamType.ToList();
+            return Json(new { data=obj});
+        }
+        public IActionResult Upsert(string id)
+        {
+            if (id == null)
+                return View(new ExamType());
+            var ex = _db.ExamType.FirstOrDefault(x => x.Id == id);
+            if (ex == null)
+                return NotFound();
+            return View(ex);
+
+        }
+        [HttpPost]
+        public IActionResult Upsert(ExamType et)
+        {
+            if (ModelState.IsValid)
+            {
+                if (et.Id == null)
+                {
+                    _db.ExamType.Add(new ExamType() { Id = Guid.NewGuid().ToString(), Name =et.Name });
+                    _db.SaveChanges();
+                    return RedirectToAction("index");
+                }
+                var oldEt = _db.ExamType.FirstOrDefault(x => x.Id == et.Id);
+                if (oldEt != null)
+                {
+                    try
+                    {
+                        oldEt.Name = et.Name;
+                        _db.ExamType.Update(oldEt);
+                        _db.SaveChanges();
+                        return RedirectToAction("index");
+                    }
+                    catch (Exception e)
+                    {
+
+                        ModelState.AddModelError("", e.Message);
+                    }
+
+                }
+                ModelState.AddModelError("", "Error");
+            }
+            return View(et);
+
+        }
+
+        public IActionResult Delete(string id)
+        {
+            try
+            {
+                var obj = _db.ExamType.Find(id);
+                _db.ExamType.Remove(obj);
+                _db.SaveChanges();
+                return Json(new { success = true, message = "Remove Class Success" });
+            }
+            catch (Exception e)
+            {
+
+                return Json(new { success = false, message = e.Message });
+            }
+        }
+    }
+}
---
+++ QuanLiSinhVien/wwwroot/js/examtype.js
@@ -0,0 +1,84 @@
+$(document).ready(function () {
+    $('#dataTable').DataTable({
+        "ajax": {
+            "url":'/admin/ExamType/getall'
+        },
+        "columns": [
+            { "data": "name" },
+            {
+                "data": "id",
+                "render": function (data) {
+                    return `
+                             <div class="text-center">
+                                <a href="/Admin/ExamType/Upsert/${data}"class="btn btn-success text-white" style="cursor:pointer">
+                                    <i class="fas fa-edit"></i>
+                                </a>
+                                <a onClick=Delete("/Admin/ExamType/Delete/${data}") class="btn btn-danger text-white" style="cursor:pointer">
+                                    <i class="fas fa-trash-alt"></i>
+                                </a>
+                            </div>                           
+                            
+                           `
+                }
+            }
+
+
+        ]
+
+    });
+}); 
+const swalWithBootstrapButtons = Swal.mixin({
+    customClass: {
+        confirmButton: 'btn btn-success',
+        cancelButton: 'btn btn-danger'
+    },
+    buttonsStyling: false
+})
+function Delete(url) {
+    swalWithBootstrapButtons.fire({
+        title: 'Are you sure?',
+        text: "You won't be able to revert this!",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonText: 'Yes, delete it!',
+        cancelButtonText: 'No, cancel!',
+        reverseButtons: true
+    }).then((result) => {
+        if (result.isConfirmed) {
+            $.ajax({
+                type: "DELETE",
+                url: url,
+                success: function (data) {
+                    if (data.success) {
+                        swalWithBootstrapButtons.fire(
+                            'Deleted!',
+                            data.message,
+                            'success'
+                        );
+                        $('#dataTable').DataTable().ajax.reload();
+                    }
+                    else {
+                        swalWithBootstrapButtons.fire(
+                            'Error',
+                            data.message,
+                            'error'
+                        )
+                    }
+                }
+
+            })
+
+        }
+        else if (result.dismiss === Swal.DismissReason.cancel) {
+            swalWithBootstrapButtons.fire(
+                'Cancelled',
+                'Your record is safe :)',
+                'error'
+            )
+        }
+    })
+}
+
+
+
+
